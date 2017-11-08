02dff658fae4ffbbcedc199b8f7b76293ed2d68c
john.zhou <john.zhou@abc360.com>
2017-11-08 14:49:50 +0800

    type: fix
    忘记提交这个了.

---
+++ src/pages/shelves/BookItem.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class BookItem extends Component {
+  handlerSelect = (book, shelf) => {
+    this.props.onUpdate(book, shelf);
+  }
+
+  render() {
+    const {book} = this.props;
+    return (
+      <div className="book">
+        <div className="book-top">
+          <div className="book-cover" style={{ width: 128, height: 193,
+            backgroundImage: `url(${book.imageLinks.thumbnail})` }}>
+            </div>
+          <div className="book-shelf-changer">
+            <select defaultValue={book.shelf ? book.shelf : 'none'} onChange={(event) => this.handlerSelect(book, event.target.value)}>
+              <option value="none" disabled>Move to...</option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
+          </div>
+        </div>
+        <div className="book-title">{book.title}</div>
+        <div className="book-authors">{book.authors.join(',')}</div>
+      </div>
+    );
+  }
+}
+
+export default BookItem;
